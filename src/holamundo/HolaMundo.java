35d5fa704369aec6ba6b55ac4dda2b3f3f453b2d
Pablo Torres Mantero <ptorres8870@ceuandalucia.es>
2022-11-29 20:00:27 +0100

    Hola mundo

--- src/holamundo/HolaMundo.java
+++ src/holamundo/HolaMundo.java
@@ -4,5 +4,6 @@ public class HolaMundo {
 public static void main(String[] args) {
 	System.out.println("Hola mundo soy Pablo");
 	System.out.println("Hola mundo soy Pablo");
+	System.out.println("Hola mundo soy Pablo");
 }
 }
